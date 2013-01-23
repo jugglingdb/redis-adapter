0d6d1bc4a4245995190067236b5e512f04950ff2
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-01-23 07:49:46 +0700

    Do not parse JSON when type String or Text, closes #183

--- test/redis.js
+++ test/redis.js
@@ -6,3 +6,19 @@ var schema = new Schema(__dirname + '/..', {});
 
 test(module.exports, schema);
 
+test.it('should not parse String as JSON', function (test) {
+    var Model = schema.define('Model', {
+        name: String,
+    });
+    var m = new Model({name: '{"property": true}'});
+    m.save(function (err, mm) {
+        test.ok(!err);
+        test.equal(mm.name, '{"property": true}');
+        Model.find(mm.id, function (err, mmm) {
+            test.ok(!err);
+            test.equal(typeof mmm.name, 'string');
+            test.equal(mmm.name, '{"property": true}');
+            test.done();
+        });
+    });
+});
